a1ea549d0f9ecff9b0fda2f696329ca9911cad06
Nick Logozzo <nlogozzo225@gmail.com>
2026-05-03 13:28:10 -0400

    Update test_services.cpp

--- tests/test_services.cpp
+++ tests/test_services.cpp
@@ -12,6 +12,21 @@ class test_base_service : public service {};
 
 class test_derived_service : public test_base_service {};
 
+class test_derived_value_service : public test_base_service
+{
+public:
+	test_derived_value_service(std::string value)
+		: m_value{ std::move(value) }
+	{
+
+	}
+
+	const std::string& get_value() const { return m_value; }
+
+private:
+	std::string m_value;
+};
+
 class test_value_service : public service
 {
 public:
@@ -54,6 +69,15 @@ TEST_F(Services_Test, ServiceCollection_addServiceAutoResolveInterfaceImpl)
 	std::shared_ptr<service_collection> sc{ std::make_shared<service_collection>() };
 	sc->add_service<test_base_service, test_derived_service>(service_scope::singleton);
 	EXPECT_TRUE(sc->contains<test_base_service>());
+}
+
+TEST_F(Services_Test, ServiceCollection_addServiceInterfaceWithArgs)
+{
+	std::shared_ptr<service_collection> sc{ std::make_shared<service_collection>() };
+	sc->add_service<test_base_service, test_derived_value_service>(service_scope::singleton, std::string("hello"));
+	std::shared_ptr<test_base_service> svc{ sc->get_service<test_base_service>() };
+	ASSERT_NE(svc, nullptr);
+	EXPECT_EQ(std::dynamic_pointer_cast<test_derived_value_service>(svc)->get_value(), "hello");
 }
 
 TEST_F(Services_Test, ServiceCollection_addServiceSingleTypeFactory)
@@ -99,5 +123,14 @@ TEST_F(Services_Test, ServiceCollection_removeService)
 {
 	m_host.services()->remove_service<notification_service>();
 	EXPECT_FALSE(m_host.services()->contains<notification_service>());
+}
+
+TEST_F(Services_Test, ServiceCollection_transientScopeCreatesNewInstances)
+{
+	std::shared_ptr<service_collection> sc{ std::make_shared<service_collection>() };
+	sc->add_service<notification_service>(service_scope::transient, std::function<std::shared_ptr<notification_service>()>([]() { return std::make_shared<notification_service>(); }));
+	std::shared_ptr<notification_service> first{ sc->get_service<notification_service>() };
+	std::shared_ptr<notification_service> second{ sc->get_service<notification_service>() };
+	EXPECT_NE(first, second);
 }
 
