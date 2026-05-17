fce89c0030ddc07494d84b7c1e3f465bd72117ed
Nick Logozzo <nlogozzo225@gmail.com>
2026-05-17 15:41:37 -0400

    Update http_service.cpp

--- tests/http_service.cpp
+++ tests/http_service.cpp
@@ -1,3 +1,4 @@
+#include <fstream>
 #include <gtest/gtest.h>
 #include <libdesktop.h>
 
@@ -5,12 +6,12 @@ using namespace desktop::filesystem;
 using namespace desktop::network;
 using namespace desktop::system;
 
-class get_data
+class httpbin_data
 {
 public:
-	get_data() = default;
+	httpbin_data() = default;
 
-	get_data(std::string origin, std::string url)
+	httpbin_data(std::string origin, std::string url)
 	    : m_origin{ std::move(origin) },
 	      m_url{ std::move(url) }
 	{
@@ -26,20 +27,35 @@ public:
 		return m_url;
 	}
 
-	friend void to_json(nlohmann::json& j, const get_data& d)
+	friend void to_json(nlohmann::json& j, const httpbin_data& d)
 	{
 		j = { { "origin", d.get_origin() }, { "url", d.get_url() } };
 	}
 
-	friend void from_json(const nlohmann::json& j, get_data& d)
+	friend void from_json(const nlohmann::json& j, httpbin_data& d)
 	{
-		d = get_data{ j.at("origin").get<std::string>(), j.at("url").get<std::string>() };
+		d = httpbin_data{ j.at("origin").get<std::string>(), j.at("url").get<std::string>() };
 	}
 
 private:
 	std::string m_origin;
 	std::string m_url;
 };
+
+TEST(HttpService, Del)
+{
+	http_service service;
+	http_response response;
+	ASSERT_NO_THROW(response = service.del("https://httpbin.org/delete"));
+	ASSERT_TRUE(response.is_success());
+	ASSERT_EQ(response.get_response_code(), 200);
+	std::optional<httpbin_data> data{ std::nullopt };
+	ASSERT_FALSE(response.get_content_as_string().empty());
+	ASSERT_NO_THROW(data = response.get_content_as_object<httpbin_data>());
+	ASSERT_TRUE(data.has_value());
+	ASSERT_EQ(data->get_url(), "https://httpbin.org/delete");
+	ASSERT_FALSE(data->get_origin().empty());
+}
 
 TEST(HttpService, DownloadFile)
 {
@@ -88,10 +104,222 @@ TEST(HttpService, Get)
 	ASSERT_NO_THROW(response = service.get("https://httpbin.org/get"));
 	ASSERT_TRUE(response.is_success());
 	ASSERT_EQ(response.get_response_code(), 200);
-	std::optional<get_data> data{ std::nullopt };
-	ASSERT_NO_THROW(data = response.get_content_as_object<get_data>());
+	std::optional<httpbin_data> data{ std::nullopt };
+	ASSERT_FALSE(response.get_content_as_string().empty());
+	ASSERT_NO_THROW(data = response.get_content_as_object<httpbin_data>());
 	ASSERT_TRUE(data.has_value());
 	ASSERT_EQ(data->get_url(), "https://httpbin.org/get");
+	ASSERT_FALSE(data->get_origin().empty());
+}
+
+TEST(HttpService, PatchJson)
+{
+	http_service service;
+	http_response response;
+	nlohmann::json body{ { "key", "value" } };
+	ASSERT_NO_THROW(response = service.patch("https://httpbin.org/patch", body));
+	ASSERT_TRUE(response.is_success());
+	ASSERT_EQ(response.get_response_code(), 200);
+	std::optional<httpbin_data> data{ std::nullopt };
+	ASSERT_FALSE(response.get_content_as_string().empty());
+	ASSERT_NO_THROW(data = response.get_content_as_object<httpbin_data>());
+	ASSERT_TRUE(data.has_value());
+	ASSERT_EQ(data->get_url(), "https://httpbin.org/patch");
+	ASSERT_FALSE(data->get_origin().empty());
+}
+
+TEST(HttpService, PatchData)
+{
+	http_service service;
+	http_response response;
+	std::vector<std::byte> bytes{ std::byte{ 0x01 }, std::byte{ 0x02 }, std::byte{ 0x03 } };
+	ASSERT_NO_THROW(response = service.patch("https://httpbin.org/patch", bytes));
+	ASSERT_TRUE(response.is_success());
+	ASSERT_EQ(response.get_response_code(), 200);
+	std::optional<httpbin_data> data{ std::nullopt };
+	ASSERT_FALSE(response.get_content_as_string().empty());
+	ASSERT_NO_THROW(data = response.get_content_as_object<httpbin_data>());
+	ASSERT_TRUE(data.has_value());
+	ASSERT_EQ(data->get_url(), "https://httpbin.org/patch");
+	ASSERT_FALSE(data->get_origin().empty());
+}
+
+TEST(HttpService, PatchFile)
+{
+	http_service service;
+	std::filesystem::path file{ user_directories::get_cache() / "test_patch.txt" };
+	std::ofstream stream{ file };
+	stream << "patch file content";
+	stream.close();
+	http_response response;
+	ASSERT_NO_THROW(response = service.patch("https://httpbin.org/patch", file));
+	ASSERT_TRUE(response.is_success());
+	ASSERT_EQ(response.get_response_code(), 200);
+	std::filesystem::remove(file);
+	std::optional<httpbin_data> data{ std::nullopt };
+	ASSERT_FALSE(response.get_content_as_string().empty());
+	ASSERT_NO_THROW(data = response.get_content_as_object<httpbin_data>());
+	ASSERT_TRUE(data.has_value());
+	ASSERT_EQ(data->get_url(), "https://httpbin.org/patch");
+	ASSERT_FALSE(data->get_origin().empty());
+}
+
+TEST(HttpService, PatchForm)
+{
+	http_service service;
+	http_response response;
+	std::vector<std::pair<std::string, std::string>> form{ { "field1", "value1" }, { "field2", "value2" } };
+	ASSERT_NO_THROW(response = service.patch("https://httpbin.org/patch", form));
+	ASSERT_TRUE(response.is_success());
+	ASSERT_EQ(response.get_response_code(), 200);
+	std::optional<httpbin_data> data{ std::nullopt };
+	ASSERT_FALSE(response.get_content_as_string().empty());
+	ASSERT_NO_THROW(data = response.get_content_as_object<httpbin_data>());
+	ASSERT_TRUE(data.has_value());
+	ASSERT_EQ(data->get_url(), "https://httpbin.org/patch");
+	ASSERT_FALSE(data->get_origin().empty());
+}
+
+TEST(HttpService, Ping)
+{
+	http_service service;
+	ASSERT_TRUE(service.ping("https://httpbin.org/get"));
+	ASSERT_FALSE(service.ping("https://invalid.url"));
+}
+
+TEST(HttpService, PostJson)
+{
+	http_service service;
+	http_response response;
+	nlohmann::json body{ { "key", "value" } };
+	ASSERT_NO_THROW(response = service.post("https://httpbin.org/post", body));
+	ASSERT_TRUE(response.is_success());
+	ASSERT_EQ(response.get_response_code(), 200);
+	std::optional<httpbin_data> data{ std::nullopt };
+	ASSERT_FALSE(response.get_content_as_string().empty());
+	ASSERT_NO_THROW(data = response.get_content_as_object<httpbin_data>());
+	ASSERT_TRUE(data.has_value());
+	ASSERT_EQ(data->get_url(), "https://httpbin.org/post");
+	ASSERT_FALSE(data->get_origin().empty());
+}
+
+TEST(HttpService, PostData)
+{
+	http_service service;
+	http_response response;
+	std::vector<std::byte> bytes{ std::byte{ 0x01 }, std::byte{ 0x02 }, std::byte{ 0x03 } };
+	ASSERT_NO_THROW(response = service.post("https://httpbin.org/post", bytes));
+	ASSERT_TRUE(response.is_success());
+	ASSERT_EQ(response.get_response_code(), 200);
+	std::optional<httpbin_data> data{ std::nullopt };
+	ASSERT_FALSE(response.get_content_as_string().empty());
+	ASSERT_NO_THROW(data = response.get_content_as_object<httpbin_data>());
+	ASSERT_TRUE(data.has_value());
+	ASSERT_EQ(data->get_url(), "https://httpbin.org/post");
+	ASSERT_FALSE(data->get_origin().empty());
+}
+
+TEST(HttpService, PostFile)
+{
+	http_service service;
+	std::filesystem::path file{ user_directories::get_cache() / "test_post.txt" };
+	std::ofstream stream{ file };
+	stream << "post file content";
+	stream.close();
+	http_response response;
+	ASSERT_NO_THROW(response = service.post("https://httpbin.org/post", file));
+	ASSERT_TRUE(response.is_success());
+	ASSERT_EQ(response.get_response_code(), 200);
+	std::filesystem::remove(file);
+	std::optional<httpbin_data> data{ std::nullopt };
+	ASSERT_FALSE(response.get_content_as_string().empty());
+	ASSERT_NO_THROW(data = response.get_content_as_object<httpbin_data>());
+	ASSERT_TRUE(data.has_value());
+	ASSERT_EQ(data->get_url(), "https://httpbin.org/post");
+	ASSERT_FALSE(data->get_origin().empty());
+}
+
+TEST(HttpService, PostForm)
+{
+	http_service service;
+	http_response response;
+	std::vector<std::pair<std::string, std::string>> form{ { "field1", "value1" }, { "field2", "value2" } };
+	ASSERT_NO_THROW(response = service.post("https://httpbin.org/post", form));
+	ASSERT_TRUE(response.is_success());
+	ASSERT_EQ(response.get_response_code(), 200);
+	std::optional<httpbin_data> data{ std::nullopt };
+	ASSERT_FALSE(response.get_content_as_string().empty());
+	ASSERT_NO_THROW(data = response.get_content_as_object<httpbin_data>());
+	ASSERT_TRUE(data.has_value());
+	ASSERT_EQ(data->get_url(), "https://httpbin.org/post");
+	ASSERT_FALSE(data->get_origin().empty());
+}
+
+TEST(HttpService, PutJson)
+{
+	http_service service;
+	http_response response;
+	nlohmann::json body{ { "key", "value" } };
+	ASSERT_NO_THROW(response = service.put("https://httpbin.org/put", body));
+	ASSERT_TRUE(response.is_success());
+	ASSERT_EQ(response.get_response_code(), 200);
+	std::optional<httpbin_data> data{ std::nullopt };
+	ASSERT_FALSE(response.get_content_as_string().empty());
+	ASSERT_NO_THROW(data = response.get_content_as_object<httpbin_data>());
+	ASSERT_TRUE(data.has_value());
+	ASSERT_EQ(data->get_url(), "https://httpbin.org/put");
+	ASSERT_FALSE(data->get_origin().empty());
+}
+
+TEST(HttpService, PutData)
+{
+	http_service service;
+	http_response response;
+	std::vector<std::byte> bytes{ std::byte{ 0x01 }, std::byte{ 0x02 }, std::byte{ 0x03 } };
+	ASSERT_NO_THROW(response = service.put("https://httpbin.org/put", bytes));
+	ASSERT_TRUE(response.is_success());
+	ASSERT_EQ(response.get_response_code(), 200);
+	std::optional<httpbin_data> data{ std::nullopt };
+	ASSERT_FALSE(response.get_content_as_string().empty());
+	ASSERT_NO_THROW(data = response.get_content_as_object<httpbin_data>());
+	ASSERT_TRUE(data.has_value());
+	ASSERT_EQ(data->get_url(), "https://httpbin.org/put");
+	ASSERT_FALSE(data->get_origin().empty());
+}
+
+TEST(HttpService, PutFile)
+{
+	http_service service;
+	std::filesystem::path file{ user_directories::get_cache() / "test_put.txt" };
+	std::ofstream stream{ file };
+	stream << "put file content";
+	stream.close();
+	http_response response;
+	ASSERT_NO_THROW(response = service.put("https://httpbin.org/put", file));
+	ASSERT_TRUE(response.is_success());
+	ASSERT_EQ(response.get_response_code(), 200);
+	std::filesystem::remove(file);
+	std::optional<httpbin_data> data{ std::nullopt };
+	ASSERT_FALSE(response.get_content_as_string().empty());
+	ASSERT_NO_THROW(data = response.get_content_as_object<httpbin_data>());
+	ASSERT_TRUE(data.has_value());
+	ASSERT_EQ(data->get_url(), "https://httpbin.org/put");
+	ASSERT_FALSE(data->get_origin().empty());
+}
+
+TEST(HttpService, PutForm)
+{
+	http_service service;
+	http_response response;
+	std::vector<std::pair<std::string, std::string>> form{ { "field1", "value1" }, { "field2", "value2" } };
+	ASSERT_NO_THROW(response = service.put("https://httpbin.org/put", form));
+	ASSERT_TRUE(response.is_success());
+	ASSERT_EQ(response.get_response_code(), 200);
+	std::optional<httpbin_data> data{ std::nullopt };
+	ASSERT_FALSE(response.get_content_as_string().empty());
+	ASSERT_NO_THROW(data = response.get_content_as_object<httpbin_data>());
+	ASSERT_TRUE(data.has_value());
+	ASSERT_EQ(data->get_url(), "https://httpbin.org/put");
 	ASSERT_FALSE(data->get_origin().empty());
 }
 
